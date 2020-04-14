dafe599b70b83541acd5d92cd8c7c878e2ef73c6
hironarita <hiro.narita@gmail.com>
2020-04-14 14:49:51 -0400

    add card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,34 @@
 import React, { useState } from 'react';
+import TextareaAutosize from 'react-textarea-autosize';
 
-export function Card() {
-    const [title, setTitle] = useState('');
+export function Card({ isAdding, addCard, title }) {
+    const [newTitle, setNewTitle] = useState('');
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter' && newTitle.length > 0) {
+            addCard(newTitle);
+        }
+    };
+
+    const handleOnBlur = () => {
+        if (newTitle.length > 0) {
+            addCard(newTitle);
+        }
+    };
 
     return (
         <div className='card trello-card'>
-            <input
-                type='text'
-                className='card-input'
-                value={title}
-                placeholder='Enter a title for this card...'
-                onChange={e => setTitle(e.target.value)} />
+            {isAdding === false && <span>{title}</span>}
+            {isAdding === true &&
+                <TextareaAutosize
+                    type='text'
+                    className='card-input'
+                    value={newTitle}
+                    placeholder='Enter a title for this card...'
+                    onChange={e => setNewTitle(e.target.value)}
+                    onKeyDown={e => handleKeyDown(e)}
+                    onBlur={() => handleOnBlur()} />
+            }
         </div>
     )
 }
--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -3,14 +3,21 @@ import { Card } from './Card';
 
 export function Column() {
     const [displayCard, setDisplayCard] = useState(false);
+    const [cards, setCards] = useState([]);
+
+    const addCard = (title) => {
+        setDisplayCard(false);
+        setCards(cards.concat([{ title }]));
+    };
 
     return (
         <div className='column'>
             <h4>Test</h4>
-            {displayCard === true && <Card />}
+            {cards.map(x => <Card isAdding={false} title={x.title} />)}
+            {displayCard === true && <Card isAdding={true} addCard={(title) => addCard(title)} />}
             <button
                 type='button'
-                className='btn btn-success mt-3'
+                className='btn btn-success mt-2'
                 onClick={() => setDisplayCard(true)}>
                 Add Card
             </button>
