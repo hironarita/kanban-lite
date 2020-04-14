956d9245dbd366fab86b660ebb4c5156a4d8ca88
hironarita <hiro.narita@gmail.com>
2020-04-14 13:07:10 -0400

    column

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Column } from './components/Column';
 
 function App() {
 	return (
-		<div className="App">
-			
+		<div className='trello-container'>
+			<Column />
 		</div>
 	);
 }
---
+++ src/components/Column.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Column() {
+    return (
+        <div className='column'>
+            <h4>Test</h4>
+        </div>
+    )
+}
