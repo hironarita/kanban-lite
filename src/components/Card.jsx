82f87495a3e21fc8cccef8997f5c73e60706bea4
uptophnarita <hiro.narita@gmail.com>
2020-04-15 13:51:55 -0400

    add card button logic

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
 
-export function Card({ isAdding, addCard, title }) {
-    const [newTitle, setNewTitle] = useState('');
-
-    const handleKeyDown = (e) => {
-        if (e.key === 'Enter' && newTitle.length > 0) {
+export function Card({ isAdding, addCard, title, setParentCardTitle, newTitle }) {
+    const handleKeyDown = (key) => {
+        if (key === 'Enter') {
             addCard(newTitle);
         }
     };
 
     const handleOnBlur = () => {
-        if (newTitle.length > 0) {
-            addCard(newTitle);
-        }
+        console.log('blur')
+        addCard(newTitle);
+    };
+
+    const handleOnChange = (title) => {
+        setParentCardTitle(title);
     };
 
     return (
@@ -22,11 +23,12 @@ export function Card({ isAdding, addCard, title }) {
             {isAdding === true &&
                 <TextareaAutosize
                     type='text'
+                    autoFocus
                     className='card-input'
                     value={newTitle}
                     placeholder='Enter a title for this card...'
-                    onChange={e => setNewTitle(e.target.value)}
-                    onKeyDown={e => handleKeyDown(e)}
+                    onChange={e => handleOnChange(e.target.value)}
+                    onKeyDown={e => handleKeyDown(e.key)}
                     onBlur={() => handleOnBlur()} />
             }
         </div>
--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -4,21 +4,31 @@ import { Card } from './Card';
 export function Column() {
     const [displayCard, setDisplayCard] = useState(false);
     const [cards, setCards] = useState([]);
+    const [cardTitle, setCardTitle] = useState('');
 
     const addCard = (title) => {
+        if (title.length > 0) {
+            setCardTitle('');
+            setCards(cards.concat([{ title }]));
+        }
         setDisplayCard(false);
-        setCards(cards.concat([{ title }]));
     };
 
     return (
         <div className='column'>
             <h4>Test</h4>
-            {cards.map(x => <Card isAdding={false} title={x.title} />)}
-            {displayCard === true && <Card isAdding={true} addCard={(title) => addCard(title)} />}
+            {cards.map((x, i) => <Card key={i} isAdding={false} title={x.title} />)}
+            {displayCard === true &&
+                <Card
+                    isAdding={true}
+                    addCard={(title) => addCard(title)}
+                    setParentCardTitle={(title) => setCardTitle(title)}
+                    newTitle={cardTitle} />
+            }
             <button
                 type='button'
                 className='btn btn-success mt-2'
-                onClick={() => setDisplayCard(true)}>
+                onClick={() => { if (displayCard === false) setDisplayCard(true) }}>
                 Add Card
             </button>
         </div>
