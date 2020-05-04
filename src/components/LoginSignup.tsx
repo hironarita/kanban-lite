734704c5d1138c13eb83493cfb3157991048692d
uptophnarita <hiro.narita@gmail.com>
2020-05-04 13:10:50 -0400

    axios response interceptor

--- src/components/LoginSignup.tsx
+++ src/components/LoginSignup.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
 import { post } from '../utilities/Axios';
 
 const headers = new Headers();
--- src/utilities/Axios.ts
+++ src/utilities/Axios.ts
@@ -1,8 +1,20 @@
 import axios from 'axios';
+import Swal from 'sweetalert2';
 
 const instance = axios.create({
     baseURL: 'http://localhost:3000',
     withCredentials: true
+});
+instance.interceptors.response.use(response => response, error => {
+    const errRes = error.response;
+    if (errRes.config.url === '/account/register' && errRes.status === 400) {
+        Swal.fire({
+            title: 'Error!',
+            text: 'Username already taken',
+            icon: 'error'
+        });
+    }
+    return Promise.reject(error);
 });
 
 export const get = async <T>(url: string) => {
