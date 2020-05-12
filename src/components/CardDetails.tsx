c1045287c85b2bb77705305ffff87f98c674c132
uptophnarita <hiro.narita@gmail.com>
2020-05-12 10:48:45 -0400

    card details wip

--- src/components/CardDetails.tsx
+++ src/components/CardDetails.tsx
@@ -4,6 +4,8 @@ import { Modal } from 'react-bootstrap';
 import TextareaAutosize from 'react-textarea-autosize';
 import { Path } from '../utilities/Enums';
 import { get } from '../utilities/Axios';
+import TitleIcon from '../images/cardTitle.svg';
+import DescriptionIcon from '../images/cardDescription.svg';
 
 export function CardDetails() {
     const textarea = useRef<any>(null);
@@ -27,19 +29,23 @@ export function CardDetails() {
         <Modal show={true} onHide={handleClose}>
             {card != null &&
                 <div>
-                    <Modal.Header closeButton>
+                    <Modal.Header className='d-flex align-items-center modal-background' closeButton>
+                        <img src={TitleIcon} alt='title icon' />
                         <TextareaAutosize
                             type='text'
                             inputRef={textarea}
-                            className='column-title'
+                            className='column-title ml-2'
                             value={cardTitle}
                             placeholder='Enter list title...'
                             onChange={(e: React.ChangeEvent<HTMLInputElement>) => setCardTitle(e.target.value)} />
                             {/* onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => handleKeyDown(e.key)}
                             onBlur={() => props.changeColumnTitle(props.column.id, columnTitle)} /> */}
                     </Modal.Header>
-                    <Modal.Body>
-
+                    <Modal.Body className='modal-background'>
+                        <div className='d-flex align-items-center'>
+                            <img src={DescriptionIcon} alt='description icon' />
+                            <span className='card-description'>Description</span>
+                        </div>
                     </Modal.Body>
                 </div>
             }
