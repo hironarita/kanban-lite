877762e4ddb4ee0873ed47f22ce2f82eed0317de
uptophnarita <hiro.narita@gmail.com>
2020-05-14 11:14:02 -0400

    build fix?

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 
+/* server configuration */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+/* static route */
 app.use(express.static(__dirname + '/build'));
 
+/* config for browser history in react */
 app.get('*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
