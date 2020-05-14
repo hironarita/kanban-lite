0cdd69339b15b2021a5fb1567bb0128b7a697f9f
uptophnarita <hiro.narita@gmail.com>
2020-05-14 10:59:20 -0400

    build

---
+++ build/precache-manifest.39129069a3cf7bdb3cc9236b81ef3208.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b7708bbb0eecba4e206942ab72d7a18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e3c5f3a052e6d3e23d3",
+    "url": "/static/js/2.87fd1cda.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.87fd1cda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f9982cc24b980dc244c",
+    "url": "/static/js/main.08b42ef9.chunk.js"
+  },
+  {
+    "revision": "9369356f59844e3d158a",
+    "url": "/static/js/runtime-main.4f1faf1f.js"
+  },
+  {
+    "revision": "e9bfb1acee115c903022f7e6964cdf9f",
+    "url": "/static/media/actions.e9bfb1ac.svg"
+  },
+  {
+    "revision": "5c571a3f8d709ffd9fc56e217e741284",
+    "url": "/static/media/cardDescription.5c571a3f.svg"
+  },
+  {
+    "revision": "7e6fdb34fe00c51d19c797315e524a1a",
+    "url": "/static/media/cardTitle.7e6fdb34.svg"
+  },
+  {
+    "revision": "7a4837d55707117fddb7847680931b3c",
+    "url": "/static/media/trash.7a4837d5.svg"
+  }
+]);
